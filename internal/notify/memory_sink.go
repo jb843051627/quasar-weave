88d0f7117485eb113dab0ea715796bb8c1190d0a
jiabo <843051627@qq.com>
2026-08-26 19:31:55 +0800

    init: quality baseline

---
+++ internal/notify/memory_sink.go
@@ -0,0 +1,40 @@
+package notify
+
+import (
+	"context"
+	"sync"
+)
+
+type MemorySink struct {
+	mu       sync.RWMutex
+	messages []Message
+}
+
+func NewMemorySink() *MemorySink {
+	return &MemorySink{messages: make([]Message, 0, 16)}
+}
+
+func (s *MemorySink) Send(ctx context.Context, message Message) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+	s.mu.Lock()
+	s.messages = append(s.messages, message)
+	s.mu.Unlock()
+	return nil
+}
+
+func (s *MemorySink) Messages() []Message {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return append([]Message(nil), s.messages...)
+}
+
+func (s *MemorySink) Last() (Message, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	if len(s.messages) == 0 {
+		return Message{}, false
+	}
+	return s.messages[len(s.messages)-1], true
+}
